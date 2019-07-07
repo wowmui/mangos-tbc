01a417d443d0ebc5f51251bd11ad5a473422c6ab
Samet Luigis <sametlol123@hotmail.com>
2019-07-08 00:29:00 +0800

    Revert "kill command should cast on player's target."
    This reverts commit 3c892119067b0eea7e949ebdce3f7af49ae1b7d6.

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -11582,8 +11582,6 @@ bool Unit::TakeCharmOf(Unit* charmed, uint32 spellId, bool advertised /*= true*/
         if (charmerPlayer->GetGroup())
             charmerPlayer->SetGroupUpdateFlag(GROUP_UPDATE_PET);
     }
-	if (spellId == 33684)
-		charmed->setFaction(1761);
 
     return true;
 }
